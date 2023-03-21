88bcfc35b4d5c9e934111a4b7ac4319fbfbcc193
Brady Bangasser <bradybbangasser@gmail.com>
2023-03-20 21:36:39 -0500

    pro

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,35 @@ mod encryption;
 use crate::encryption::encryption as Encryption;
 use clap::{Command};
 
-fn cli() -> Command {
-    return Command::new("clown");
-}
-
 fn main() {
-    let matches = cli().get_matches()
-
-    println!("{:?}", args);
-    println!("{}", Encryption::encrypt(&args.key, &args.message).unwrap());
-    // println!("{}", Encryption::decrypt(&"hello world", &Encryption::encrypt(&"hello world", &"HELLO WORLD").unwrap()));
+    let cmd = clap::Command::new("clowncryption")
+        .bin_name("clown")
+        .subcommand_required(true)
+        .subcommand(
+            clap::command!("encrypt").arg(
+                clap::arg!([MESSAGE])
+                    .value_parser(clap::value_parser!(std::path::PathBuf))
+                    .help("The message to encrypt")
+            ).about("Encrypt a message")
+        )
+        .subcommand(
+            clap::command!("decrypt").arg(
+                clap::arg!([KEY])
+                    .value_parser(clap::value_parser!(std::path::PathBuf))
+                    .help("The key for the encrypted message")
+                    .required(true)
+            ).arg(
+                clap::arg!([MESSAGE])
+                    .value_parser(clap::value_parser!(std::path::PathBuf))
+                    .help("The encrypted message")
+                    .required(true)
+            ).about("PENIS")
+        );
+    let matches = cmd.get_matches();
+    let matches = match matches.subcommand() {
+        Some(("encrypt", matches)) => matches,
+        _ => unreachable!("clap should ensure we don't get here"),
+    };
+    let manifest_path = matches.get_one::<std::path::PathBuf>("manifest-path");
+    println!("{:?}", manifest_path);
 }
