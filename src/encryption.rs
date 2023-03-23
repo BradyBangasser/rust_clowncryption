2a8fed7fc8df10e8d617d752bac000920e5d82b7
Brady Bangasser <bradybbangasser@gmail.com>
2023-03-23 10:47:19 -0500

    finished

--- src/encryption.rs
+++ src/encryption.rs
@@ -6,26 +6,48 @@ pub mod encryption {
     };
     use base64::Engine;
 
-    pub fn encrypt(key: &str, s: &str) -> Result<String, aes_gcm::Error> {
+    pub fn encrypt(key: &str, s: &str, nonce: &mut Vec<u8>, encode: bool, binary: bool) -> Result<String, aes_gcm::Error> {
         let cipher = create_cipher(key);
-        let nonce = Nonce::from_slice(b"plaintext me"); // 96-bits; unique per message
+
+        nonce.resize(12, 0);
+        let nonce = Nonce::from_slice(&nonce);
+
         let ciphertext = cipher.encrypt(nonce, s.as_ref())?;
+
         let mut output = Vec::new();
         output.resize(ciphertext.len() * 4 / 3 + 4, 0);
         let bytes_written = base64::engine::general_purpose::STANDARD.encode_slice(ciphertext.clone(), &mut output).unwrap();
         output.truncate(bytes_written);
+
         let output_string = hex::encode(String::from_utf8(output).unwrap());
+
+        if encode {
+            return Ok(convert_to_clown(&output_string, binary))
+        }
+
         return Ok(output_string)
     }
 
-    pub fn decrypt(key: &str, s: &str) -> String {
+    pub fn decrypt(key: &str, s: &str, nonce: &mut Vec<u8>, encode: bool, binary: bool) -> String {
+
+        let mut mutable_string = s.to_string();
+
+        if encode {
+            mutable_string = convert_from_clown(&mutable_string, binary);
+        }
+
         let cipher = create_cipher(key);
-        let nonce = Nonce::from_slice(b"plaintext me");
-        let base64_vec = hex::decode(s).unwrap();
+
+        nonce.resize(12, 0);
+        let nonce = Nonce::from_slice(&nonce);
+
+        let base64_vec = hex::decode(mutable_string).unwrap();
         let mut encrypted_bytes = Vec::new();
         encrypted_bytes.resize(base64_vec.len() * 4 / 3 + 4, 0);
+
         let decoded_bytes = base64::engine::general_purpose::STANDARD.decode_slice(base64_vec, &mut encrypted_bytes).unwrap();
         encrypted_bytes.truncate(decoded_bytes);
+
         let ciphertext = cipher.decrypt(nonce, encrypted_bytes.as_ref()).unwrap();
         let text = String::from_utf8(ciphertext).unwrap();
         return text;
@@ -37,5 +59,47 @@ pub mod encryption {
         mutable_vec.resize(32, 0);
         let arr: [u8; 32]  = mutable_vec.try_into().unwrap();
         return Aes256GcmSiv::new(arr.as_slice().into())
+    }
+
+    fn get_clown_convertion() -> [(&'static str, &'static str); 16] {
+        return [("a", "🥸"), ("b", "🥛"), ("c", "🗿"), ("d", "🤨"), ("e", "😐"), ("f", "😏"), ("0", "🤡"), ("1", "🤓"), ("2", "🫁"), ("3", "🤯"), ("4", "📮"), ("5", "🐄"), ("6", "🥌"), ("7", "💩"), ("8", "🤠"), ("9", "🥴")];
+    }
+
+    fn convert_to_clown(source_string: &str, binary: bool) -> String {
+        let mut target_string: String = hex::encode(source_string);
+        if binary {
+            let hex_string = target_string.clone();
+            target_string = "".to_string();
+            for character in hex_string.into_bytes() {
+                target_string += &format!("{:0>8}", format!("{:b}", character))
+            }
+
+        }
+
+        for conversion in get_clown_convertion() {
+            target_string = target_string.replace(conversion.0, conversion.1)
+        }
+        
+
+        return target_string.to_string()
+    }
+
+    fn convert_from_clown(source: &str, binary: bool) -> String {
+        let mut declowned_string = source.to_string();
+        let mut target_string;
+
+        for conversion in get_clown_convertion() {
+            declowned_string = declowned_string.replace(conversion.1, conversion.0);
+        }
+
+        if binary {
+            target_string = String::from_utf8((0..declowned_string.len()).step_by(8).map(|i| u8::from_str_radix(&declowned_string[i..i + 8], 2).unwrap()).collect()).unwrap();
+        } else {
+            target_string = declowned_string
+        }
+
+        target_string = String::from_utf8(hex::decode(target_string).unwrap()).unwrap();
+
+        return target_string
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,91 @@
 mod encryption;
 
 use crate::encryption::encryption as Encryption;
-use clap::{Command};
+use clap;
 
 fn main() {
     let cmd = clap::Command::new("clowncryption")
         .bin_name("clown")
         .subcommand_required(true)
         .subcommand(
-            clap::command!("encrypt").arg(
-                clap::arg!([MESSAGE])
-                    .value_parser(clap::value_parser!(std::path::PathBuf))
-                    .help("The message to encrypt")
-            ).about("Encrypt a message")
-        )
-        .subcommand(
-            clap::command!("decrypt").arg(
-                clap::arg!([KEY])
-                    .value_parser(clap::value_parser!(std::path::PathBuf))
+            clap::command!("encrypt")
+            .arg(
+                clap::arg!(key: [KEY])
+                    .value_parser(clap::value_parser!(String))
                     .help("The key for the encrypted message")
                     .required(true)
-            ).arg(
-                clap::arg!([MESSAGE])
-                    .value_parser(clap::value_parser!(std::path::PathBuf))
+            )
+            .arg_required_else_help(true).arg(
+                clap::arg!(message: [MESSAGE])
+                    .value_parser(clap::value_parser!(String))
+                    .help("The message to encrypt")
+                    .required(true)
+            )
+            .about("Encrypt a message")
+            .arg_required_else_help(true)
+            .arg(clap::arg!(binary: -b --binary)
+            .action(clap::ArgAction::SetTrue)
+            .help("Encode message in binary"))
+            .arg(
+                clap::arg!(nonce: --nonce <NONCE>)
+                .help("Sets the Nonce")
+                .default_value("nonce")
+            )
+            .arg(clap::arg!(encode: -d --dontencode)
+            .action(clap::ArgAction::SetFalse)
+            .help("Don't encode the message in clowns"))
+
+        )
+        .subcommand(
+            clap::command!("decrypt")
+            .arg(
+                clap::arg!(key: [KEY])
+                    .value_parser(clap::value_parser!(String))
+                    .help("The key for the encrypted message")
+                    .required(true)
+            )
+            .arg_required_else_help(true)
+            .arg(
+                clap::arg!(message: [MESSAGE])
+                    .value_parser(clap::value_parser!(String))
                     .help("The encrypted message")
                     .required(true)
-            ).about("PENIS")
+            )
+            .arg_required_else_help(true)
+            .about("Decrypt a message")
+            .arg(clap::arg!(binary: -b --binary)
+            .action(clap::ArgAction::SetTrue)
+            .help("Decodes message from binary"))
+            .arg(
+                clap::arg!(nonce: --nonce <NONCE>)
+                .help("Sets the Nonce")
+                .default_value("nonce")
+            )
+            .arg(clap::arg!(encode: -d --dontencode)
+            .action(clap::ArgAction::SetFalse)
+            .help("Don't encode the message in clowns"))
         );
+
+
     let matches = cmd.get_matches();
-    let matches = match matches.subcommand() {
-        Some(("encrypt", matches)) => matches,
+    let encrypted_string: String = match matches.subcommand() {
+        Some(("encrypt", matches)) => {
+            let key = matches.get_one::<std::string::String>("key").unwrap();
+            let message = matches.get_one::<std::string::String>("message").unwrap();
+            let binary = matches.get_one::<bool>("binary").unwrap();
+            let encode = matches.get_one::<bool>("encode").unwrap();
+            let nonce = matches.get_one::<std::string::String>("nonce").unwrap();
+            Encryption::encrypt(key, message, &mut nonce.as_bytes().to_vec(), *encode, *binary).unwrap()
+        },
+        Some(("decrypt", matches)) => {
+            let key = matches.get_one::<std::string::String>("key").unwrap();
+            let message = matches.get_one::<std::string::String>("message").unwrap();
+            let binary = matches.get_one::<bool>("binary").unwrap();
+            let nonce = matches.get_one::<std::string::String>("nonce").unwrap();
+            let encode = matches.get_one::<bool>("encode").unwrap();
+            Encryption::decrypt(key, message, &mut nonce.as_bytes().to_vec(), *encode, *binary)
+        },
         _ => unreachable!("clap should ensure we don't get here"),
     };
-    let manifest_path = matches.get_one::<std::path::PathBuf>("manifest-path");
-    println!("{:?}", manifest_path);
+    println!("{}", encrypted_string);
 }
